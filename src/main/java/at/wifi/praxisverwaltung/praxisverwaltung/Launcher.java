971d9b0c0d219734b04755cb6b3ad7b9263355d6
biancablds <boldusbiancadaniela@gmail.com>
2026-05-09 11:15:02 +0200

    login first edit

--- src/main/java/at/wifi/praxisverwaltung/praxisverwaltung/Launcher.java
+++
@@ -1,9 +0,0 @@
-package at.wifi.praxisverwaltung.praxisverwaltung;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module at.wifi.praxisverwaltung.praxisverwaltung {
     requires javafx.fxml;
 
 
-    opens at.wifi.praxisverwaltung.praxisverwaltung to javafx.fxml;
-    exports at.wifi.praxisverwaltung.praxisverwaltung;
+    opens at.wifi.praxisverwaltung to javafx.fxml;
+    exports at.wifi.praxisverwaltung;
 }
