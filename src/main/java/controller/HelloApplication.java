75c350d3588cb7a1da82c978a188055568dd1c64
biancablds <boldusbiancadaniela@gmail.com>
2026-05-15 18:56:57 +0200

    Feinschliff Desgin LoginSeite, Logo erstellt

--- src/main/java/controller/HelloApplication.java
+++ src/main/java/controller/HelloApplication.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login");
+        stage.setTitle("Hausarztpraxis Dr.in med. Gia Guerra");
         stage.setScene(scene);
         stage.setMaximized(true);
+        stage.getIcons().add(
+                new Image(getClass().getResourceAsStream("/pictures/logo-ausgeschnitten.png"))
+        );
+
         stage.show();
     }
 }
--- src/main/java/database/testDB.java
+++ src/main/java/database/testDB.java
@@ -1,14 +1,31 @@
 package database;
 
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class testDB {
-    public static void main(String[] args) {
-        try (Connection con = DBManager.getConnection()) {
-            System.out.println("MariaDB Verbindung erfolgreich!");
-        } catch (SQLException e) {
-            System.out.println("Verbindung fehlgeschlagen");
+
+    public static void main() {
+
+        String sql = "SELECT * FROM employees";
+
+        try (
+                Connection con = DBManager.getConnection();
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()
+        ) {
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt(1) + " | " +
+                                rs.getString(2) + " | " +
+                                rs.getString(3)
+                );
+            }
+
+        } catch (Exception e) {
+            System.out.println("Fehler beim Lesen");
             e.printStackTrace();
         }
     }
